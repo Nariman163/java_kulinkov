74c60b31f22067c30e3d9c9fa4688ad7a6a8e475
Nariman <nariman@MacBook-Pro-Nariman.local>
2026-06-25 18:30:20 +0300

    Refactor

---
+++ sandbox/src/main/java/gnm/geometry/Geometry.java
@@ -0,0 +1,13 @@
+package gnm.geometry;
+
+import gnm.geometry.figures.Rectangle;
+import gnm.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(5);//Указали класс функции и далее саму функцию(через точку)
+
+        Rectangle.printRectangleArea(7,8);
+    }
+
+}
---
+++ sandbox/src/main/java/gnm/geometry/figures/Square.java
@@ -0,0 +1,11 @@
+package gnm.geometry.figures;
+
+public class Square {
+    public static void printSquareArea(double a){
+        System.out.println("Площадь квадрата со стороной " + a + " = " + squareArea(a));
+    }
+
+    private static double squareArea(double a){
+        return a * a;
+    }
+}
